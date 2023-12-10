2d372ca4c75dc9fe9523f33a472be4a4de99b093
Fatma Bolat <fatmabolat58@gmail.com>
2023-12-10 20:50:47 +0300

    changes on navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
           <h1>Kitap Kurdu</h1>
         </div>
         {/* SEARCH BAR */}
-        <div className="w-2/5">
+        <div className="w-2/5 text-center">
           <form>
             <label className="text-navbar font-medium">Ara:</label>
             <input
@@ -17,7 +17,7 @@ function Home() {
               name="searchBar"
               className="border-2 border-black pt-1 pb-1 pl-6 pr-6 rounded-2xl w-3/5 ml-3"
             />
-            <button className="border-2 border-black pt-2 pb-2 pl-3 pr-3 rounded-2xl ml-3">
+            <button className="border-2 border-black pt-2 pb-2 pl-3 pr-3 rounded-2xl ml-3 hover:bg-slate-300 delay-150">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="12"
@@ -33,10 +33,10 @@ function Home() {
         </div>
         {/* SIGN IN/ SIGN UP */}
         <div>
-          <button className="border-2 border-navbar bg-navbar text-gray-300 pt-1 pb-1 pl-5 pr-5 rounded-xl ml-3 text-sm">
+          <button className="border-2 border-navbar bg-navbar hover:bg-red-600 hover:border-red-600 hover:scale-110 text-gray-300 pt-1 pb-1 pl-5 pr-5 rounded-xl ml-3 text-sm">
             Üye Ol
           </button>
-          <button className="border-2 border-navbar bg-navbar text-gray-300 pt-1 pb-1 pl-5 pr-5 rounded-xl ml-3 text-sm">
+          <button className="border-2 border-navbar bg-navbar hover:bg-red-600 hover:border-red-600 hover:scale-110  text-gray-300 pt-1 pb-1 pl-5 pr-5 rounded-xl ml-3 text-sm">
             Giriş Yap
           </button>
         </div>
@@ -46,23 +46,41 @@ function Home() {
       {/* CATEGORIES START */}
       <div className="text-center align-middle mt-5 p-6 bg-lime-600 text-gray-100">
         <div className="flex justify-evenly w-full font-bold text-lg text-center ">
-          <div>Bilim Kurgu</div>
+          <div>
+            <a>Bilim Kurgu</a>
+          </div>
           <div className="h-6 w-px bg-gray-300"></div>
-          <div>Fantastik</div>
+          <div>
+            <a>Fantastik</a>
+          </div>
           <div className="h-6 w-px bg-gray-300"></div>
-          <div>Macera</div>
+          <div>
+            <a>Macera</a>
+          </div>
           <div className="h-6 w-px bg-gray-300"></div>
-          <div>Romantik</div>
+          <div>
+            <a>Romantik</a>
+          </div>
           <div className="h-6 w-px bg-gray-300"></div>
-          <div>Polisiye</div>
+          <div>
+            <a>Polisiye</a>
+          </div>
           <div className="h-6 w-px bg-gray-300"></div>
-          <div>Klasikler</div>
+          <div>
+            <a>Klasikler</a>
+          </div>
           <div className="h-6 w-px bg-gray-300"></div>
-          <div>Şiir</div>
+          <div>
+            <a>Şiir</a>
+          </div>
           <div className="h-6 w-px bg-gray-300"></div>
-          <div>Çizgi Roman</div>
+          <div>
+            <a>Çizgi Roman</a>
+          </div>
           <div className="h-6 w-px bg-gray-300"></div>
-          <div>İletişim</div>
+          <div>
+            <a>İletişim</a>
+          </div>
         </div>
       </div>
       {/* CATEGORIES END */}
