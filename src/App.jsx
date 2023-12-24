4180996749582e6264df08473f30e3850d51ce52
Fatma Bolat <fatmabolat58@gmail.com>
2023-12-24 15:34:36 +0300

    login

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import AdminLogin from "./components/AdminLogin";
 import Home from "./components/Home";
 import KullaniciSozlesmesi from "./components/KullaniciSozlesmesi";
 import SignUp from "./components/SignUp";
+import Login from "./components/Login";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/signUp" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/adminGiris" element={<AdminLogin />} />
         <Route path="/kullaniciSozlesmesi" element={<KullaniciSozlesmesi />} />
       </Routes>
