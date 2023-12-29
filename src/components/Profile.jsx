82c62f9c355b766fb7bf552e2710d6aa946626c4
Tutku Usumus <tutkusumus@gmail.com>
2023-12-29 18:06:52 +0300

    profile page

---
+++ src/components/Profile.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { selectAuth } from "../Store/authSlice";
+
+function Profile() {
+  const { currentUser } = useSelector(selectAuth);
+
+  return (
+    <div className="h-screen w-screen bg-loginBack flex flex-col items-center justify-center text-slate-100">
+      <h2 className="text-title text-2xl font-bold mb-4">Hesap Bilgilerim</h2>
+      <div className="bg-loginBack border-2 border-title p-8 rounded-lg shadow-md w-80">
+        <p className="text-title text-white mb-2">Ad: {currentUser.firstName}</p>
+        <p className="text-title text-white mb-2">Soyad: {currentUser.lastName}</p>
+        <p className="text-title text-white mb-2">E-posta: {currentUser.email}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
